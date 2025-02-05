95a41d5e2a279160180fb43890474b944e1f9b89
Esther-Omono <enitanatakere@gmail.com>
2025-02-05 04:31:00 +0100

    Added feature to handle state-related operations

---
+++ js/gameState.js
@@ -0,0 +1,41 @@
+export class gameState {
+    constructor() {
+        this.score = 0;
+        this.colors = [];
+        this.targetColor = '';
+    }
+
+    incrementScore() {
+        this.score++;
+        return this.score;
+    }
+
+    resetScore() {
+        this.score = 0;
+        return this.score;
+    }
+
+    setColors(colors) {
+        this.colors = colors;
+    }
+
+    setTargetColor(color) {
+        this.targetColor = color;
+    }
+
+    isCorrectGuess(color) {
+        return color === this.targetColor;
+    }
+
+    getScore() {
+        return this.score;
+    }
+
+    getColors() {
+        return this.colors;
+    }
+
+    getTargetColor() {
+        return this.targetColor;
+    }
+}
